b4bdebbec3f1469f2fc807af036f9d9a0f6127fb
rohit357 <sushmadevi04139@gmail.com>
2026-07-20 14:31:29 +0530

    new changes

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ print("This is a new line of code added to the test.py file.")
 
 print("new line")
 
+print("This is another new line of code added to the test.py file.")
+
+def func():
+    print("This is a function in the test.py file.")
+
+func()
+
